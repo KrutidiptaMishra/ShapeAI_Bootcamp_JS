4f326a952e89c356d10f835bbc403e8e90d2e7c0
KrutidiptaMishra <85435468+KrutidiptaMishra@users.noreply.github.com>
2021-06-06 15:08:38 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../styles.css"
+
+function Header() {
+  return (
+    <header className="head">
+      <h1>ShapeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
